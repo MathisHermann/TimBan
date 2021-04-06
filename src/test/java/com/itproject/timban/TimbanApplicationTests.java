4600a22c3ce521c9a74f48619a43e3f5a372c53d
Mathis Hermann <mathis.hermann@students.fhnw.ch>
2021-04-06 15:14:27 +0200

    Initial commit 2

---
+++ src/test/java/com/itproject/timban/TimbanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itproject.timban;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimbanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
