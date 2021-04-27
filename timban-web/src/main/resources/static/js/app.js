63f2f99c670353d2d7cf819c47b45fd8106af396
Lars Stegmann <lars.stegmann@students.fhnw.ch>
2021-04-27 16:56:05 +0200

    login validation + dashboard

---
+++ timban-web/src/main/resources/static/js/app.js
@@ -0,0 +1,47 @@
+
+<!-- START PART LARS-->
+
+<!-- COPIED & ADJUSTED FROM INTERNET TECHNOLOGY PROJECT -->
+
+function login(email, password, callback) {
+
+    $.ajax({
+        type: "POST",
+        contentType: "application/json",
+        headers: {
+            "X-XSRF-TOKEN": getCookie("XSRF-TOKEN")
+        },
+        url: serviceEndpointURL + "/login",
+        data: JSON.stringify({
+            "email": email,
+            "password": password
+        }),
+        success: function (data, textStatus, response) {
+            callback(true);
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log(jqXHR, textStatus, errorThrown);
+            callback(false);
+        }
+    });
+
+}
+
+function validateLogin(callback) {
+    $.ajax({
+        type: "HEAD",
+        url: serviceEndpointURL + "/validate",
+        success: function (data, textStatus, response) {
+            callback(true);
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            callback(false);
+        }
+    });
+}
+
+function preventDefault(){
+
+}
+
+<!-- END PART LARS-->
