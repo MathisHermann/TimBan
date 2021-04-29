49936a1731923ed9988e7e9a803f4b5ca7be385b
Lars Stegmann <lars.stegmann@students.fhnw.ch>
2021-04-29 12:01:17 +0200

    added current time and current date function

--- timban-web/src/main/resources/static/js/app.js
+++ timban-web/src/main/resources/static/js/app.js
@@ -44,4 +44,10 @@ function preventDefault(){
 
 }
 
+function currentTime () {
+    var today = new Date();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    return currentTime;
+}
+
 <!-- END PART LARS-->
