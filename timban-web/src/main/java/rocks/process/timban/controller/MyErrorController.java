bdfbb1bd23919d735875dff34429773d688cfcac
Mathis Hermann <mathis.hermann@students.fhnw.ch>
2021-05-21 16:31:55 +0200

    Error pages and some Styling.

---
+++ timban-web/src/main/java/rocks/process/timban/controller/MyErrorController.java
@@ -0,0 +1,46 @@
+package rocks.process.timban.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import rocks.process.timban.business.service.TimbanUserService;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class MyErrorController implements ErrorController {
+
+    @Autowired
+    TimbanUserService timbanUserService;
+
+    @RequestMapping("/error")
+    public String renderErrorPage(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        if (status != null) {
+            Integer statusCode = Integer.valueOf(status.toString());
+            switch (statusCode) {
+                case 404:
+                    return "404";
+                case 403:
+                    return "403";
+                default:
+                    try {
+                        if (timbanUserService.getCurrentTimbanUser() == null)
+                            return "redirect:/dashboard";
+                        else
+                            return "login";
+                    } catch (Exception e) {
+                        return "login";
+                    }
+            }
+        }
+        return "login";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return null;
+    }
+}
