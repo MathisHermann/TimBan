2854f318b7be3191c05325b8d132449edda3f5e5
Mathis Hermann <mathis.hermann@students.fhnw.ch>
2021-05-28 18:00:08 +0200

    wip

--- timban-web/src/main/java/rocks/process/timban/business/service/OnStartService.java
+++ timban-web/src/main/java/rocks/process/timban/business/service/OnStartService.java
@@ -41,7 +41,7 @@ public class OnStartService implements ApplicationListener<ApplicationReadyEvent
     @Autowired
     ReportPDF reportPDF;
 
-    private final boolean runsProductive = Boolean.getBoolean(System.getenv("RUN_PROD"));
+    private final boolean runsProductive = System.getenv("RUN_PROD").equals("prod");
     private boolean adminCreated = false;
     private boolean noProjectCreated = false;
     private boolean fakeUsersCreated = false;
